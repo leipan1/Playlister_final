769e7396de4a12357b51e41867da3b2c58331df8
Leila Pan <leila.pan@stonybrook.edu>
2022-11-29 23:38:01 -0500

    splash page with working buttons except guest login

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,7 +1,30 @@
+import { Link } from 'react-router-dom'
 export default function SplashScreen() {
+
     return (
         <div id="splash-screen">
-            Playlister
+            <div id="splash-screen-text">
+                Welcome to
+                <div id="splash-screen-playlister-text">
+                    Playlister
+                </div>
+                Your go-to playlist organizer
+            </div>
+            <div id="splash-screen-logins">
+                <Link to='/register/'>
+                <button id="splash-screen-create-account">
+                    Create Account
+                </button>
+                </Link>
+                <Link to='/login/'>
+                <button id="splash-screen-login">
+                    Login
+                </button>
+                </Link>
+            </div>
+            <button id="splash-screen-continue-as-guest">
+                    Continue as Guest
+            </button>
         </div>
     )
 }
